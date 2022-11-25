8b711bbecbd79308cbad6c5cd08ffc03acd75b82
nampd <nampd@hpt.vn>
2022-11-25 16:57:00 +0700

    daily 15 minutes last day

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
 function App() {
   return (
-    <div className="App">
-        namdz
+    <div className="flex gap-8 h-screen items-center bordder border-red-500 justify-center">
+      <input
+        type="text"
+        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+      ></input>
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
     </div>
   );
 }
