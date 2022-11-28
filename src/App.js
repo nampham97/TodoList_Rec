2914a58432360f8b2392a5c90a446fdec56bb60c
nampd <nampd@hpt.vn>
2022-11-28 17:21:51 +0700

    updated onChange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [work, setWork] = useState("");
+  const handleAdd = () => {
+    console.log("aoma");
+  };
   return (
     <div className="flex gap-8 h-screen items-center bordder border-red-500 justify-center">
       <input
         type="text"
         className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+        value={work}
+        onChange={(e) => setWork(e.target.value)}
       ></input>
       <button
         type="button"
         className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+        onClick={() => handleAdd}
       >
         Add
       </button>
+      <div className="border-blue-400"></div>
     </div>
   );
 }
